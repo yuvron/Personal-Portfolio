1c4591458950c4abf8382fa190117c3d546a42f6
yuvron <yuvr99@gmail.com>
2022-06-21 02:27:34 +0300

    Added search bar for stocks search and search results

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,5 +1,26 @@
 import { Stock } from "./stock";
+import { searchValue } from "./fetch";
 
-const stock = new Stock("AAPL", 100, 100);
+// const stock = new Stock("AAPL", 100, 100);
 
-setTimeout(() => (document.querySelector(".stock").innerHTML = stock.toString()), 3000);
+// setTimeout(() => (document.querySelector(".stock").innerHTML = stock.toString()), 3000);
+const searchContainer = document.getElementById("search-container");
+const searchBox = document.getElementById("search-box") as HTMLInputElement;
+
+searchBox.addEventListener("blur", () => document.querySelectorAll(".search-item").forEach((searchItem) => searchItem.classList.add("hidden")));
+searchBox.addEventListener("focus", () => document.querySelectorAll(".search-item").forEach((searchItem) => searchItem.classList.remove("hidden")));
+
+searchBox.addEventListener("input", async () => {
+	const searchTerm = searchBox.value;
+	[...searchContainer.children].forEach((searchItem) => searchItem.remove());
+	searchValue(searchTerm)
+		.then((data) => {
+			data.forEach((stock) => {
+				const searchItem = document.createElement("p");
+				searchItem.classList.add("search-item");
+				searchItem.innerHTML = stock;
+				searchContainer.appendChild(searchItem);
+			});
+		})
+		.catch((err) => console.log("ERROR: ", err));
+});
--- src/scripts/fetch.ts
+++ src/scripts/fetch.ts
@@ -7,6 +7,7 @@ const options = {
 };
 
 export function getStockDetails(ticker: string): Promise<{ name: string; price: number; dividendYield: number }> {
+	options.headers["X-RapidAPI-Host"] = "yh-finance.p.rapidapi.com";
 	return fetch(`https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=${ticker}&region=US`, options)
 		.then((response) => response.json())
 		.then((data) => {
@@ -24,4 +25,12 @@ export async function getStockPrice(ticker: string): Promise<number> {
 		.then((response) => response.json())
 		.then((data) => +data.price)
 		.catch((err) => err);
+}
+
+export async function searchValue(search: string): Promise<Array<string>> {
+	options.headers["X-RapidAPI-Host"] = "stock-data2.p.rapidapi.com";
+	return fetch(`https://stock-data2.p.rapidapi.com/v6/finance/autocomplete?query=${search}&lang=en`, options)
+		.then((response) => response.json())
+		.then((data) => data.ResultSet.Result.map((stock) => stock.symbol).slice(0, 5))
+		.catch((err) => console.log("fetch error:", err));
 }
