fca20315310fef7f5d2b2d4579da0c87204415b3
yuvron <yuvr99@gmail.com>
2022-06-21 10:41:41 +0300

    When a search item is clicked a new stock with its symbol is created

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -4,23 +4,34 @@ import { searchValue } from "./fetch";
 // const stock = new Stock("AAPL", 100, 100);
 
 // setTimeout(() => (document.querySelector(".stock").innerHTML = stock.toString()), 3000);
-const searchContainer = document.getElementById("search-container");
+const searchContainer = document.querySelector(".search-container");
+const searchResults = document.getElementById("search-results");
 const searchBox = document.getElementById("search-box") as HTMLInputElement;
+const stocksContainer = document.querySelector(".stocks");
 
 searchBox.addEventListener("blur", () => document.querySelectorAll(".search-item").forEach((searchItem) => searchItem.classList.add("hidden")));
 searchBox.addEventListener("focus", () => document.querySelectorAll(".search-item").forEach((searchItem) => searchItem.classList.remove("hidden")));
 
 searchBox.addEventListener("input", async () => {
 	const searchTerm = searchBox.value;
-	[...searchContainer.children].forEach((searchItem) => searchItem.remove());
+	[...searchResults.children].forEach((searchItem) => searchItem.remove());
 	searchValue(searchTerm)
 		.then((data) => {
 			data.forEach((stock) => {
 				const searchItem = document.createElement("p");
 				searchItem.classList.add("search-item");
 				searchItem.innerHTML = stock;
-				searchContainer.appendChild(searchItem);
+				searchItem.addEventListener("click", () => addNewStock(stock));
+				searchResults.appendChild(searchItem);
 			});
 		})
 		.catch((err) => console.log("ERROR: ", err));
 });
+
+function addNewStock(stock: string): void {
+	const newStock = new Stock(stock, 100, 100);
+	const element = document.createElement("p");
+	element.classList.add("stock");
+	setTimeout(() => (element.innerHTML = newStock.toString()), 3000);
+	stocksContainer.appendChild(element);
+}
